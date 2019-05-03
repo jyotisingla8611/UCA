122cd9cbd2cad5c38006ddf4e208b9a80ca6fc63
jyotisingla8611 <jyotisingla8611@gmail.com>
2019-05-03 23:57:22 +0530

    WordRank added another method

--- ADI/src/Practice/wordRank.java
+++ ADI/src/Practice/wordRank.java
@@ -56,3 +56,61 @@ public class wordRank{
         System.out.print(sum);
      }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Another Method
+/*import java.util.*;
+public class HelloWorld{
+
+     static int fact(int n){
+         int f = 1;
+         while(n > 1){
+             f = f * n;
+             n--;
+         }
+         return f;
+     }
+     static int min_count(String s){
+         int cnt = 0;
+         for(int j = 0 ;j < s.length() ;j++){
+             if(s.charAt(j) != '$')
+               cnt++;
+         }
+         return cnt;
+         
+     }
+     public static void main(String []args){
+        String str = "take";
+        char[] arr = str.toCharArray();
+        Arrays.sort(arr);
+        int sum = 0;
+        String temp = new String(arr);
+        int len = str.length();
+        System.out.println(temp);
+        for(int i = 0 ; i < len  ;i++){
+            for(int j = 0 ; j < len ;j++){
+                if(str.charAt(i) == temp.charAt(j)){
+                    temp = temp.replace(str.charAt(i),'$');
+                    break;
+                }else if(temp.charAt(j)!='$'){
+                    int x = min_count(temp);
+                    System.out.print(x + " x");
+                    sum = sum + fact(x-1);
+                }
+            }
+            
+        }
+        System.out.print(sum);
+     }
+}*/
